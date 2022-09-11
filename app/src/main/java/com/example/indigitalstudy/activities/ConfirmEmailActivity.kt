a60aba3c60ccd912538dc01a3427cdf5af296b2c
Reporterxxx <mikhailbiruykov@mail.ru>
2022-09-11 16:34:15 +0300

    new_edit_text_in_confirm_email

---
+++ app/src/main/java/com/example/indigitalstudy/activities/ConfirmEmailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.indigitalstudy.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.indigitalstudy.R
+
+class ConfirmEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_email)
+    }
+}
